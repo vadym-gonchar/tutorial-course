c99a3584a638edbafd8cea6c392f9943311f3d32
vadym <vadym.gonchar@gmail.com>
2020-04-09 17:37:00 +0300

    add first changes

---
+++ src/test/java/tutor/MyFirstProgram.java
@@ -0,0 +1,18 @@
+package tutor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        WebDriver driver1 = new FirefoxDriver();
+        WebDriver driver2 = new SafariDriver();
+        WebDriver driver3 = new OperaDriver();
+        WebDriver driver4 = new EdgeDriver();
+    }
+}
