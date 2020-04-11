78f73c94cdd2ef33c632e28405c21fe713394456
vadym <vadym.gonchar@gmail.com>
2020-04-12 02:58:06 +0300

    add 3 browsers job

--- src/test/java/tutor/MyFirstProgram.java
+++ src/test/java/tutor/MyFirstProgram.java
@@ -3,6 +3,7 @@ package tutor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MyFirstProgram {
@@ -10,11 +11,24 @@ public class MyFirstProgram {
 
     @Test(priority = 1)
     public void firstTest() {
-        driver = new ChromeDriver();
-        driver.get("https://github.com/login");
-        driver.manage().window().maximize();
-        driver.findElement(By.cssSelector("[id='login_field']")).sendKeys("vadym-gonchar");
 
+        driver = new ChromeDriver();
+        mainJob();
+
+        driver = new FirefoxDriver();
+        mainJob();
+
+        /*driver = new InternetExplorerDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        mainJob();*/
+    }
+
+    public void mainJob() {
+        driver.get("https://github.com/login");
+        driver.manage().window().fullscreen();
+        driver.findElement(By.name("login")).sendKeys("aqa-tutoring");
+        driver.findElement(By.name("password")).sendKeys("aqatest123");
+        driver.findElement(By.name("commit")).click();
         driver.quit();
     }
 }
