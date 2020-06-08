e8d193a3053584afaa51817d34249f385a42430d
Vadym Gonchar <vadym.gonchar@gmail.com>
2020-06-08 19:04:47 +0300

    add patch and delete methods

--- src/main/java/helpers/apihelper/GitHubApiHelper.java
+++ src/main/java/helpers/apihelper/GitHubApiHelper.java
@@ -1,10 +1,22 @@
 package helpers.apihelper;
 
 import io.restassured.http.ContentType;
-
 import static io.restassured.RestAssured.given;
 
 public class GitHubApiHelper {
+
+  public String getAndCheckRepoParam(String baseUrl, String requestNewUrl, String token, String pathToParam) {
+    return given().header("Authorization", "Bearer " + token)
+            .when()
+            .get(baseUrl + requestNewUrl)
+            .then().log().all()
+            .log().cookies()
+            .statusCode(200)
+            .extract()
+            .response()
+            .jsonPath()
+            .getString(pathToParam);
+  }
 
   public void createNewRepo(String baseUrl, String requestNewUrl,
                             String token, String newRepoName, String description){
@@ -29,16 +41,35 @@ public class GitHubApiHelper {
             .statusCode(201);
   }
 
-  public String getRepos(String baseUrl, String requestNewUrl, String token, String pathToParam) {
-    return given().header("Authorization", "Bearer " + token)
+  public void updateRepo(String baseUrl, String requestNewUrl,
+                            String token, String newRepoName, String description){
+
+    String updateRepoBody = "{\n" +
+            "  \"name\": \"" + newRepoName + "\",\n" +
+            "  \"description\": \"" + description + "\",\n" +
+            "  \"homepage\": \"" + baseUrl + "\",\n" +
+            "  \"private\": false,\n" +
+            "  \"has_issues\": true,\n" +
+            "  \"has_projects\": true,\n" +
+            "  \"has_wiki\": true\n" +
+            "}";
+
+    given().header("Authorization", "Bearer " + token)
+            .contentType(ContentType.JSON)
+            .body(updateRepoBody)
             .when()
-            .get(baseUrl + requestNewUrl)
+            .patch(baseUrl + requestNewUrl)
             .then().log().all()
             .log().cookies()
-            .statusCode(200)
-            .extract()
-            .response()
-            .jsonPath()
-            .getString(pathToParam);
+            .statusCode(200);
+  }
+
+  public void deleteRepo(String baseUrl, String requestNewUrl, String token){
+    given().header("Authorization", "Bearer " + token)
+            .when()
+            .delete(baseUrl + requestNewUrl)
+            .then().log().all()
+            .log().cookies()
+            .statusCode(204);
   }
 }
--- src/test/java/apitest/GitHubApiTest.java
+++ src/test/java/apitest/GitHubApiTest.java
@@ -4,17 +4,30 @@ import helpers.apihelper.GitHubApiHelper;
 import org.testng.Assert;
 
 public class GitHubApiTest{
-  public final static String TOKEN = "7c07f721ac21eebdb3c75848eb791a43dc34fcc9";
+  public final static String TOKEN = "173cf48a20fb19d2ac1e42921748337d242577f2";
   private final static String URL = "https://api.github.com/";
 
   public static void main(String[] args) {
     GitHubApiHelper gitHubApiHelper = new GitHubApiHelper();
 
-    gitHubApiHelper.createNewRepo(URL, "user/repos",
-            TOKEN, "aNewRepoNameqqqdA", "Test description");
-
-    String htmlUrl = gitHubApiHelper.getRepos(URL,
+    String htmlUrlTutorialCourse = gitHubApiHelper.getAndCheckRepoParam(URL,
             "repos/vadym-gonchar/tutorial-course", TOKEN, "html_url" );
-    Assert.assertEquals(htmlUrl, "https://github.com/vadym-gonchar/tutorial-course");
+    Assert.assertEquals(htmlUrlTutorialCourse, "https://github.com/vadym-gonchar/tutorial-course");
+
+    String idAddressbook = gitHubApiHelper.getAndCheckRepoParam(URL,
+            "repos/vadym-gonchar/addressbook", TOKEN, "id" );
+    Assert.assertEquals(idAddressbook, "249262256");
+
+    String ownerIdPgatour = gitHubApiHelper.getAndCheckRepoParam(URL,
+            "repos/vadym-gonchar/pgatour", TOKEN, "owner.id" );
+    Assert.assertEquals(ownerIdPgatour, "61626216");
+
+    gitHubApiHelper.createNewRepo(URL, "user/repos",
+            TOKEN, "Test", "Test description");
+
+    gitHubApiHelper.updateRepo(URL, "repos/vadym-gonchar/Test",
+            TOKEN, "TestRepository", "Test description update");
+
+    gitHubApiHelper.deleteRepo(URL, "repos/vadym-gonchar/TestRepository", TOKEN);
   }
 }
